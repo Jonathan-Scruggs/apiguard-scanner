61b2a8dfa13e98ee047b919fba1bb9c97ec29427
Jonathan Scruggs <30415863+Jonathan-Scruggs@users.noreply.github.com>
2025-08-15 10:56:30 -0600

    updating requirements + getting started on cli

--- src/core/parsers/openapi_parser.py
+++ src/core/parsers/openapi_parser.py
@@ -1,21 +1,20 @@
 """
 Parser Is A Class That can Be Instantiated. Contains methods for parsing the OPENAPI spec.
-
 """
-
-
 from ast import List
 from typing import Dict
 from core.types import APIEndpoint, APISpec
-
+import yaml
 
 class OpenAPIParser:
     def __init__(self, file_path: str):
-        pass
+        self.spec_path = file_path
+        self.spec_data = None # Raw Spec Data
+        self.components = {} # Spec Resuable parts like security schemas and object schemas
 
     def parse(self) -> APISpec:
         '''
-        High Level Primary Method to Parse the APISpec 
+        Public High Level Primary Method to Parse the APISpec 
         '''
         pass
 
@@ -23,7 +22,26 @@ class OpenAPIParser:
         '''
         Internal class method used to load(open) the spec and perform basic validation on it.
         '''
-        pass
+        try:
+            with open('example-spec.yaml','r') as file: 
+                if self.spec_path.endswith("yaml") or self.spec_path.endswith("yml"):
+                    data = yaml.load(file, Loader=yaml.SafeLoader)
+                else:
+                    raise ValueError("Invalid file format, please provide a  yml or yaml file.")
+
+        except FileNotFoundError:
+            print(f"Error: the file at path:{self.spec_path} wasn't found")
+        
+        except yaml.YAMLError as exc:
+            print(f"Something went wrong parsing the YAML file: {exc}")
+
+        # Checking to verify the the data is now in a dictionary format
+        if not isinstance(data,dict):
+            raise ValueError(f"The specified file {self.spec_path} wasn't successfully parsed into a dict")
+        
+        # Checking to Verify the data parsed is a OPEN API API Spec and is Version 3.0+ 
+        print(data)
+
 
     def _extract_components(self):
         '''
